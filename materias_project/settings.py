f8112e3dc86234eeedd48b7a5361e39398933a81
GerardoBedollaR <l21120175@morelia.tecnm.mx>
2025-12-11 18:30:46 -0600

    Se cambio la bd a postgres

--- materias_project/settings.py
+++ materias_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,10 +78,27 @@ WSGI_APPLICATION = 'materias_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'materias.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
+# Si existen las variables de entorno, usar PostgreSQL
+if all([
+    os.environ.get("DB_NAME"),
+    os.environ.get("DB_USER"),
+    os.environ.get("DB_PASSWORD"),
+    os.environ.get("DB_HOST"),
+]):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.environ.get("DB_NAME"),
+            'USER': os.environ.get("DB_USER"),
+            'PASSWORD': os.environ.get("DB_PASSWORD"),
+            'HOST': os.environ.get("DB_HOST"),
+            'PORT': os.environ.get("DB_PORT", "5432"),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
